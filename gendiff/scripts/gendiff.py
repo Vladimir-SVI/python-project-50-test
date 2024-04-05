4789fd6d735b8c1da9cc208f472c16a269a738d7
SVI <Svinovoch@yandex.ru>
2024-04-05 09:10:44 +0300

    difference calculator lesson two

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,6 +8,7 @@ def parse_args():
                                          and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
     args = parser.parse_args()
     print(args)
 
